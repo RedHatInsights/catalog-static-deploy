100cfc96945bbf6fdc2a41e2601cab1b6b880572
Travis CI User <travis@example.org>
2021-09-03 14:57:55 +0000

    Merge pull request #1059 from lgalis/update_component_titles_type
    Change title type to PageHeaderTitle

--- js/240.9ba4fb0869e5c1713708.js
+++ js/240.9ba4fb0869e5c1713708.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcatalog=self.webpackChunkcatalog||[]).push([[240],{20240:(e,n,t)=>{t.d(n,{Z:()=>r});var u=t(30624);const r=function(){var e=(0,u.useRef)(!1);return(0,u.useEffect)((function(){return e.current=!0,function(){return e.current=!1}}),[]),e}}}]);
-//# sourceMappingURL=../sourcemaps/240.3a48d7b8c8152bc1f9e7.js.map
+//# sourceMappingURL=../sourcemaps/240.5d71e3c9d26894a20606.js.map
