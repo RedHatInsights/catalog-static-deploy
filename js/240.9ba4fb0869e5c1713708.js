62f0fe44f318da1dd4053fbe751fd374019afce9
Travis CI User <travis@example.org>
2021-09-03 15:00:56 +0000

    Merge pull request #1052 from lgalis/standalone_webpack_config
    Add standalone webpack configuration

--- js/240.9ba4fb0869e5c1713708.js
+++ js/240.9ba4fb0869e5c1713708.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkcatalog=self.webpackChunkcatalog||[]).push([[240],{20240:(e,n,t)=>{t.d(n,{Z:()=>r});var u=t(30624);const r=function(){var e=(0,u.useRef)(!1);return(0,u.useEffect)((function(){return e.current=!0,function(){return e.current=!1}}),[]),e}}}]);
-//# sourceMappingURL=../sourcemaps/240.5d71e3c9d26894a20606.js.map
+//# sourceMappingURL=../sourcemaps/240.2c25a06b13d6ee191fd7.js.map
