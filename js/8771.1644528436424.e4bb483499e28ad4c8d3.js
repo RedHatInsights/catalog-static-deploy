ffbda51ceb15fa48a634c5d58c3279de794b83fc
Travis CI User <travis@example.org>
2022-02-10 21:28:59 +0000

    Update and rename dist-release.yml to test.yml

---
+++ js/8771.1644528436424.e4bb483499e28ad4c8d3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcatalog=self.webpackChunkcatalog||[]).push([[8771],{28412:(e,t,a)=>{a.r(t),a.d(t,{default:()=>o});var l=a(30624),n=a.n(l),c=a(14494),r=a(75122),u=a(98054),s=a(29558);const o=function(){return n().createElement(c.zt,{store:(0,r.Z)(!0)},n().createElement(s.Z,{locale:"en"},n().createElement(u.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/8771.3a963d0432fabdf2d1514e2d31ac22d0.js.map
